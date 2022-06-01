edb283c5902e06f1a9e82976501b0769254156e8
Ricardo Peixe <101753069+ricardopeixe@users.noreply.github.com>
2022-06-01 11:19:30 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,22 +1,19 @@
 import cv2
 
-camera = cv2.VideoCapture(0)  # leitura da câmara
-
+camera = cv2.VideoCapture(0)    # cam (n) : {0, 1, ... n}
 
 if __name__ == '__main__':   
     while True:
         ret, frame = camera.read()  
         if ret is True:
             roi = frame[250:300, 0:640]
-
-
+            
             cv2.imshow('frame', frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('s'):
+            if cv2.waitKey(1) & 0xFF == ord('s'):   # 's' to break
                 break
         else:
             print('Câmara não detectada')
             break
-        
+            
 camera.release()
 cv2.destroyAllWindows()
